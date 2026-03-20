5cb2e54a451e9ba3b51c75b7b264b97d45bc1fab
Jean Ceugniet <jean.ceugniet@gmail.com>
2026-03-20 11:06:47 +0100

    ci(CI-001): set up GitHub Actions CI pipeline

---
+++ frontend/eslint.config.js
@@ -0,0 +1,20 @@
+import js from '@eslint/js'
+import vue from 'eslint-plugin-vue'
+import tseslint from 'typescript-eslint'
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...vue.configs['flat/recommended'],
+  {
+    files: ['**/*.vue', '**/*.ts'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
+  {
+    ignores: ['dist/', 'node_modules/'],
+  },
+]

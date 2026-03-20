68e96833a74bb36f6352a64e043fb16dbe0eb0bb
Jean Ceugniet <jean.ceugniet@gmail.com>
2026-03-20 10:34:39 +0100

    chore(CHORE-001): initialize NestJS backend project

---
+++ backend/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AlphabetModule } from './alphabet/alphabet.module';
+import { CipherModule } from './cipher/cipher.module';
+import { RotationModule } from './rotation/rotation.module';
+import { KeyModule } from './key/key.module';
+import { ReadingOrderModule } from './reading-order/reading-order.module';
+import { RendererModule } from './renderer/renderer.module';
+import { ValidationModule } from './validation/validation.module';
+import { ApiModule } from './api/api.module';
+
+@Module({
+  imports: [AlphabetModule, CipherModule, RotationModule, KeyModule, ReadingOrderModule, RendererModule, ValidationModule, ApiModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
